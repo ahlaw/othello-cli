c05030760fe3454983ae665d33775dea2aaef373
Amos Law <amos.law98@gmail.com>
2020-04-30 13:02:33 -0400

    Add type annotations to noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,15 @@
 import tempfile
+from typing import Any
 
 import nox
+from nox.sessions import Session
 
 
 nox.options.sessions = "lint", "safety", "mypy", "tests"
 locations = "othello", "tests", "noxfile.py"
 
 
-def install_with_constraints(session, *args, **kwargs):
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -21,14 +23,14 @@ def install_with_constraints(session, *args, **kwargs):
 
 
 @nox.session(python="3.8")
-def black(session):
+def black(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python=["3.8", "3.7"])
-def lint(session):
+def lint(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(
         session,
@@ -43,7 +45,7 @@ def lint(session):
 
 
 @nox.session(python="3.8")
-def safety(session):
+def safety(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -61,7 +63,7 @@ def safety(session):
 
 
 @nox.session(python=["3.8", "3.7"])
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
@@ -69,7 +71,7 @@ def tests(session):
 
 
 @nox.session(python=["3.8", "3.7"])
-def mypy(session):
+def mypy(session: Session) -> None:
     args = session.posargs or locations
     install_with_constraints(session, "mypy")
     session.run("mypy", *args)
